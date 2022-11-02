b670e4b3e7d512308cb50074153af6031a7d774a
Karit08 <karla.jm.ingmec49@gmail.com>
2022-11-01 22:46:18 -0600

    Database

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -3,10 +3,49 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
+  sequelize.define('dog', 
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    min_heightMin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    max_height: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    min_weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    max_weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    life_span:{
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    img_url: {
+      type: DataTypes.STRING,
+      defaultValue: 'https://placedog.net/640/480?random',
+      allowNull: true,
+    },
+    is_db:{
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
+    }
+  }, {
+    timestamps: false,
+  },);
 };
